19d8415584227faf302d5630f2262166fe105f3a
ercelserhat <ercelserhat@gmail.com>
2023-02-03 22:52:19 +0300

    Homepage Design

---
+++ GuessTheNumber/SonucEkraniVC.swift
@@ -0,0 +1,29 @@
+//
+//  SonucEkraniVC.swift
+//  GuessTheNumber
+//
+//  Created by Serhat on 3.02.2023.
+//
+
+import UIKit
+
+class SonucEkraniVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
